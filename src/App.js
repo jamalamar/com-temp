6fbd08f85ce21de476a0eaf6cf2856c180fbc7cc
jamalamar <jamal_ab22@hotmail.com>
2019-06-30 20:54:14 -0500

    reset

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
-import React from 'react'
-import { hot } from 'react-hot-loader/root'
-import { Route, Router, withSiteData } from 'react-static'
-import { Switch } from 'react-router'
-// import Routes from 'react-static-routes'
+import React from 'react';
+import './App.css';
+import HomepageLayout from './Views/landing.js'
 
-import Sidebar from './Components/Sidebar'
-import style from './Style'
-import { docTypes } from '/utils'
-
-const App = ({ componentMenu, versions }) => (
-  <div style={style.container}>
-    <Router>
-      <React.Fragment>
-        <Switch>
-          {/*
-           * We can't place <Sidebar /> inside of <Routes /> because it will be remounted on page
-           * switch. We also don't want to show <Sidebar /> for layouts pages and maximized pages.
-           */}
-          <Route exact path='/layouts/:name' component={null} />
-          <Route path='/maximize/*' component={null} />
-
-          <Route path='/'>
-             {(props) => (
-               <Sidebar
-                 {...props}
-                 componentMenu={componentMenu}
-                 style={style.menu}
-                 version={versions.suir}
-               />
-             )}
-          </Route>
-        </Switch>
-
-        
-      </React.Fragment>
-    </Router>
-  </div>
-)
-
-App.propTypes = {
-  componentMenu: docTypes.componentMenu.isRequired,
-  versions: docTypes.versions,
+function App() {
+  return (
+    <div className="App">
+      <HomepageLayout/>
+    </div>
+  );
 }
 
-export default hot(withSiteData(App))
+export default App;
