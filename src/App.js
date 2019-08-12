7677419c91fc3674a00fe220d1897a1ee9755839
jamalamar <jamal_ab22@hotmail.com>
2019-08-12 12:54:25 -0500

    created router and about component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import Style from './App.css'
 import Homepage from './Views/HomePage.js'
+import About from './Views/About.js'
+
 
 function App() {
   return (
-    <div className="App">
-      <Homepage/>
-    </div>
+	<Router>
+	    <div className="App">
+
+	      	<Route path="/" exact component={Homepage}	/>
+	      	<Route path="/about"  component={About}	/>
+    	
+	    </div>
+	</Router>
   );
 }
 
 export default App;
+
