2a76eb695021a06b116f981823b8a063aeb2948f
jamalamar <jamal_ab22@hotmail.com>
2019-08-13 18:26:47 -0500

    modified app.js to a class component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Style from './App.css'
 import Homepage from './Views/HomePage.js'
+import Nosotros from './Views/Nosotros.js'
 import Automovil from './Views/Automovil.js'
 import Medico from './Views/Medico.js'
 import Vida from './Views/Vida.js'
-import Nosotros from './Views/Nosotros.js'
 
 
-function App() {
+class App extends Component {
+	
+	state = {
+		logged: false
+	}
+
+render(){
   return (
 	<Router>
 	    <div className="App">
 
 	      	<Route path="/" exact component={Homepage}	/>
+	      	<Route path="/nosotros"  component={Nosotros}	/>
 	      	<Route path="/automovil"  component={Automovil}	/>
 	      	<Route path="/medico"  component={Medico}	/>
 	      	<Route path="/vida"  component={Vida}	/>
-	      	<Route path="/nosotros"  component={Nosotros}	/>
     	
 	    </div>
 	</Router>
   );
+}
 }
 
 export default App;
